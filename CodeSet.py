f5ae26d7cb4689931b556fa2344ceeec67a62c48
Jiravat Boonkumnerd <jo06942@gmail.com>
2017-03-30 18:13:10 +0700

    up

---
+++ CodeSet.py
@@ -0,0 +1,11 @@
+
+class CodeSet:
+
+    def __init__(self, name, charSet):
+        """
+        
+        :param name : Name of Codeset
+        :param charSet : List of Character Hex Code 
+        """
+        self.name = name
+        self.charSet = charSet
--- UnicodeSet.py
+++
@@ -1,6 +0,0 @@
-
-class CodeSet:
-
-    def __init__(self, name, charNums):
-        self.name = name
-        self.charNums = charNums
